3c9306a6259ab49cc464a8b349d66d9f927c14e8
Narek Charchyan <narek1985ch@gmail.com>
2021-01-10 03:54:14 +0100

    Commit - 2 // create components "Registration" and "Login"

--- myNote/src/app/app-routing.module.ts
+++ myNote/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MyNoteComponent } from './myNote/myNote.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'myNote', component: MyNoteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myNote/src/app/app.module.ts
+++ myNote/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MyNoteComponent } from './myNote/myNote.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [	
+      AppComponent,
+      RegistrationComponent,
+      LoginComponent,
+      MyNoteComponent
+   ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
